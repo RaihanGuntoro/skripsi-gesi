bd4fbb07256c084bce03b6c06b0a1ac96535f17d
Raihan Anugerah Guntoro <raihanguntoro22@gmail.com>
2023-03-29 14:27:10 +0700

    login

--- app/Models/user.php
+++ app/Models/user.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-
+        'telephone_number',
     ];
 
     /**
@@ -32,7 +32,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-      
     ];
 
     /**
